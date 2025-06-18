e2bb85a14f8b3de560195ae166dcf7c44e045e7e
GLaura22 <97692344+GLaura22@users.noreply.github.com>
2025-06-18 20:36:12 +0200

    Update send_dynamic.py

--- send_dynamic.py
+++ send_dynamic.py
@@ -4,16 +4,23 @@ from time import sleep
 import random
 from config import get_network_config, RIFO
 
+# ip fejlec utan a rifo (rang) kovetkezik
 bind_layers(IP, RIFO)
+
+# halozati konfig betoltese
 cfg = get_network_config()
 
 for i in range(100):
+      # csomag rangjanak random generalasa
       rank = random.randint(0, 65000)
+
+      # csomag keszitese
       pkt = Ether(src=cfg["src_mac"], dst=cfg["dst_mac"]) / \
             IP(src=cfg["src_ip"], dst=cfg["dst_ip"]) / \
             RIFO(rank=rank) / \
             Raw(load=f"Random rank={rank}")
 
+      # csomag kuldese
       sendp(pkt, iface=cfg["iface"], verbose=False)
       print(f"[{i}] Packet sent with rank={rank} from {cfg['src_ip']} -> {cfg['dst_ip']} on {cfg['iface']}")
       sleep(0.5)
