6191a2f659b0e357693b2656668f8ae957d92bdf
GLaura22 <97692344+GLaura22@users.noreply.github.com>
2025-06-18 20:55:15 +0200

    Update receive_sync.py

--- receive_sync.py
+++ receive_sync.py
@@ -7,10 +7,13 @@ from config import get_network_config, RIFO
 
 bind_layers(IP, RIFO)
 cfg = get_network_config()
+
+# szalbiztos tarolasa a csomagoknak
 pkt_queue = queue.Queue()
 counter = 0
 lock = threading.Lock()
 
+# csomagok feldolgozasa
 def worker():
     global counter
     while True:
@@ -26,10 +29,13 @@ def worker():
                 print(f"[{counter}] Packet received with rank={rank} from {src} -> {dst} on {cfg['iface']}")
         pkt_queue.task_done()
 
+# 15 szalon fut egyszerre a csomagfeldolgozas
 for _ in range(15):
     threading.Thread(target=worker, daemon=True).start()
 
+# beerkezo csomagok betoltese a queueba
 def enqueue(pkt):
     pkt_queue.put(pkt)
 
+# csomagkovetes
 sniff(iface=cfg["iface"], prn=enqueue, store=0)
