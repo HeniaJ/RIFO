8dc678cf19f177b72d5894f9c68f12ad9d6fc59f
GLaura22 <97692344+GLaura22@users.noreply.github.com>
2025-06-18 20:43:16 +0200

    Update send_multiple.py

--- send_multiple.py
+++ send_multiple.py
@@ -3,11 +3,18 @@ from scapy.packet import bind_layers
 import time
 from config import get_network_config, RIFO
 
+# csomag fejleceben IP utan a rang kovetkezik
 bind_layers(IP, RIFO)
+
+# halozati konfiguracio betoltese
 cfg = get_network_config()
 iface = cfg["iface"]
+
+# rang lista tobb csomag kuldesenek tesztelesehez
+# ezek az ertekek lesznek beallitva a csomagok rangjanak
 ranks = [10, 11, 9, 15, 4]
 
+# rangsor ertekekkel ellatott csomagok kuldese fel masodperc kesleltetessel
 for r in ranks:
       pkt = Ether(src=cfg["src_mac"], dst=cfg["dst_mac"]) / \
             IP(src=cfg["src_ip"], dst=cfg["dst_ip"]) / \
