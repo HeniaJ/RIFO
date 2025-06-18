581cd4eacd88116e1dcf3e7c75878b2549737783
GLaura22 <97692344+GLaura22@users.noreply.github.com>
2025-06-18 23:02:54 +0200

    Update run_more_hosts.sh

--- run_more_hosts.sh
+++ run_more_hosts.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 mkdir -p script_run
 
+# h1 fogado hosztra beerkezett csomagok logjainak eltarolasa
 echo "[+] Starting receiver on h1..."
 mx h1 python3 receive_log.py 2>&1 | tee script_run/receiver.log &
 
 sleep 5
 
+# 9 hoszt kuldi a csomagokat parhuzamosan
 for i in $(seq 2 10)
 do
     host="h$i"
