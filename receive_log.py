06befee4b3e7ad5692687175758a322d10ef5a95
GLaura22 <97692344+GLaura22@users.noreply.github.com>
2025-06-18 20:50:00 +0200

    Update receive_log.py

--- receive_log.py
+++ receive_log.py
@@ -3,13 +3,16 @@ from scapy.layers.inet import IP
 from scapy.packet import bind_layers
 from config import get_network_config, RIFO
 
+# halozati forgalom figyelese
+
 bind_layers(IP, RIFO)
 cfg = get_network_config()
 counter = 0
 
 def handle(pkt):
     global counter
-    
+
+    # csomag infok kiirasa monitorozashoz
     if RIFO in pkt and pkt[IP].src != cfg["src_ip"]:
         counter += 1
         rank = pkt[RIFO].rank
@@ -17,4 +20,5 @@ def handle(pkt):
         dst = pkt[IP].dst
         print(f"[{counter}] Packet received with rank={rank} from {src} -> {dst} on {cfg['iface']}")
 
+# csomagfigyeles elinditasa
 sniff(iface=cfg["iface"], prn=handle, store=0)
